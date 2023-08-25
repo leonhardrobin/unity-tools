0475457b3f80813232a62d117ac34248d29ed9cb
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2023-08-25 12:29:42 +0200

    Add Assembly Definition for editor folder

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -4,7 +4,7 @@ using static UnityEngine.Application;
 using static System.IO.Directory;
 using static System.IO.Path;
 
-namespace leonhardrobin
+namespace leonhardrobin.editorTools
 {
     public static class ToolsMenu
     {
