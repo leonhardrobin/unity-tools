a760e351677b07d9886048b801aa5186564c6c4e
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2024-01-16 11:38:49 +0100

    Add old singleton for preview users

---
+++ Singleton/OldSingleton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace LRS.Singleton
+{
+    /// <summary>
+    /// A singleton class that can be inherited from to create a singleton.
+    /// </summary>
+    /// <typeparam name="T">The class that inherits from this singleton</typeparam>
+    public class OldSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                _instance = FindFirstObjectByType<T>();
+                if (_instance == null)
+                {
+                    Debug.LogError("An instance of " + typeof(T) +
+                                   " is needed in the scene, but there is none.");
+                }
+
+                return _instance;
+            }
+        }
+
+        protected bool useDontDestroyOnLoad = false;
+
+        public static bool IsInitialized => _instance != null;
+
+        protected virtual void Awake()
+        {
+            if (this != Instance)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this as T;
+                if (useDontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
