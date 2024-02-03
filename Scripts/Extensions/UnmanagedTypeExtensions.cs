96af01aa40ebc2dcb879eeb85237ef7095e6a583
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2024-02-03 21:59:15 +0100

    Add Extension to check whether a type is unmanaged or not

---
+++ Scripts/Extensions/UnmanagedTypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LRS.Utils
+{
+    public static class UnmanagedTypeExtensions
+    {
+        private class U
+        {
+        }
+
+        public static bool IsUnManaged(this Type t)
+        {
+            try
+            {
+                Type makeGenericType = typeof(U).MakeGenericType(t);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
